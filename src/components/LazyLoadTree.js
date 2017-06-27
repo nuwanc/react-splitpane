20b88bca479f12c420dea3d167b98932b86edc37
Nuwan Chandrasoma <nuwan.chandrasoma@gtnexus.com>
2017-06-27 15:33:33 +0530

    updated

---
+++ src/components/LazyLoadTree.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+class LazyLoadTree extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+            loaded: false,
+            childNodes: null
+        };
+        this.toggle = this.toggle.bind(this);
+        this.nodeClick = this.nodeClick.bind(this);
+    }
+
+    toggle() {
+        let childNodes = null;
+        if (!this.state.loaded) {
+            if (this.props.node.childNodes) {
+                childNodes = this.props.node.childNodes.map((node, index) => {
+                    return <li key={index}><LazyLoadTree node={node} root={false} onTreeNodeSelect={this.props.onTreeNodeSelect} /></li>
+                });
+                this.setState(() => {
+                    return {
+                        visible: !this.state.visible,
+                        childNodes: childNodes,
+                        loaded: true
+                    }
+                });
+            }
+        } else {
+            this.setState(() => {
+                return {
+                    visible: !this.state.visible
+                }
+            });
+        }
+
+    }
+
+    nodeClick(node) {
+        this.props.onTreeNodeSelect(node);
+    }
+
+    render() {
+        let classObj;
+        let selectedObj;
+
+        classObj = {
+            "minus": this.state.visible,
+            "plus": !this.state.visible
+        };
+
+        let style;
+        if (!this.state.visible) {
+            style = { display: "none" };
+        }
+
+
+        selectedObj = {
+            "fa": true,
+            "node": true,
+        }
+        //if root, show immediate childNode
+        let root;
+        let childNodes = null;
+
+        if (this.props.root) {
+            if (this.props.node.childNodes != null) {
+                childNodes = this.props.node.childNodes.map((node, index) => {
+                    return <li key={index}><LazyLoadTree node={node} root={false} onTreeNodeSelect={this.props.onTreeNodeSelect} /></li>
+                });
+            }
+            style = { display: "block" };
+        } else {
+            if (this.props.node.title) {
+                root = <span onClick={this.toggle} className={classNames(classObj)}></span>;
+            } else {
+                root = '';
+            }
+        }
+
+        return (
+            <div>
+                {root}
+                <i className={this.props.node.icon} aria-hidden="true">&nbsp;</i>
+                <i className={classNames(selectedObj)} onClick={this.props.onTreeNodeSelect.bind(null, this.props.node.path)}>{this.props.node.title}</i>
+                <ul className='SimpleTree' style={style}>
+                    {childNodes || this.state.childNodes}
+                </ul>
+            </div>
+        );
+    }
+
+}
+
+LazyLoadTree.propTypes = {
+    node: PropTypes.object.isRequired,
+    onTreeNodeSelect: PropTypes.func.isRequired
+}
+
+export default LazyLoadTree;
