c2cef9bdd2ba4c61708dedc672c07d05ffcd734e
Nuwan Chandrasoma <nuwan.chandrasoma@gtnexus.com>
2017-07-04 12:11:49 +0530

    updated

--- src/utils/EdiHelper.js
+++ src/utils/EdiHelper.js
@@ -2,26 +2,15 @@ import Store from './Store';
 
 function getSegments(message, ignoreTransactions) {
     let segments = [];
-    let segment = {};
 
     if (message.ISA != null) {
         //is interchange
         let isa = message.ISA;
-        segment = {};
-        segment.name = isa.n;
-        segment.path = isa.p;
-        segment.element = isa.e;
-        segment.schema = Store.lookupHeaderPath(isa.p);
-        segments.push(segment);
+        segments.push(processSegment(isa));
         let groups = message.group.length;
         for (let i = 0; i < groups; i++) {
             let gs = message.group[i].GS;
-            segment = {};
-            segment.name = gs.n;
-            segment.path = gs.p;
-            segment.element = gs.e;
-            segment.schema = Store.lookupHeaderPath(gs.p);
-            segments.push(segment);
+            segments.push(processSegment(gs));
             if (!ignoreTransactions) {
                 let transactions = message.group[i].transaction.length;
                 for (let j = 0; j < transactions; j++) {
@@ -30,29 +19,14 @@ function getSegments(message, ignoreTransactions) {
                 }
             }
             let ge = message.group[i].GE;
-            segment = {};
-            segment.name = ge.n;
-            segment.path = ge.p;
-            segment.element = ge.e;
-            segment.schema = Store.lookupHeaderPath(ge.p);
-            segments.push(segment);
+            segments.push(processSegment(ge));
         }
         let iea = message.IEA;
-        segment = {};
-        segment.name = iea.n;
-        segment.path = iea.p;
-        segment.element = iea.e;
-        segment.schema = Store.lookupHeaderPath(iea.p);
-        segments.push(segment);
+        segments.push(processSegment(iea));
 
     } else if (message.GS != null) {
         let gs = message.GS;
-        segment = {};
-        segment.name = gs.n;
-        segment.path = gs.p;
-        segment.element = gs.e;
-        segment.schema = Store.lookupHeaderPath(gs.p);
-        segments.push(segment);
+        segments.push(processSegment(gs));
         if (!ignoreTransactions) {
             let transactions = message.transaction.length;
             for (let j = 0; j < transactions; j++) {
@@ -61,17 +35,23 @@ function getSegments(message, ignoreTransactions) {
             }
         }
         let ge = message.GE;
-        segment = {};
-        segment.name = ge.n;
-        segment.path = ge.p;
-        segment.element = ge.e;
-        segment.schema = Store.lookupHeaderPath(ge.p);
-        segments.push(segment);
+        segments.push(processSegment(ge));
     } else if (message.ST != null) {
         processSegments(message.c, segments);
     }
 
     return segments;
+}
+
+function processSegment(inSegment) {
+    let segment = {};
+
+    segment.name = inSegment.n;
+    segment.path = inSegment.p;
+    segment.element = inSegment.e;
+    segment.schema = Store.lookupHeaderPath(inSegment.p);
+
+    return segment;
 }
 
 function processSegments(children, segments) {
