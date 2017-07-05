c551149181f07d4beace3e70067ebb431887e412
Nuwan Chandrasoma <nuwan.chandrasoma@gtnexus.com>
2017-07-05 23:40:03 +0530

    updated

--- src/components/SegmentTab.js
+++ src/components/SegmentTab.js
@@ -4,6 +4,10 @@ import * as EdiHelper from '../utils/EdiHelper';
 
 class SegmentTab extends Component {
 
+    renderOption(option) {
+        return option.value + "=" +option.description;
+    } 
+
     render() {
         let node = this.props.segment.schema;
         let index = this.props.index;
@@ -12,11 +16,27 @@ class SegmentTab extends Component {
 
         node.element.forEach((v,i)=>{
             let details = EdiHelper.getSchemaDetails(v.name);
+            let select;
+            if (details.name.startsWith("code")) {
+                let options = [];
+                options = details.value.map((v,i)=>{
+                    return <option value={v.value}>{v.value} = {v.description}</option>
+                })
+                options.unshift(<option value="">[empty]</option>);
+                select = <select value={this.props.segment.element[i]} style={{width:'400px'}}>{options}</select>
+            } else {
+                if (this.props.segment.element[i]) {
+                    select = <span>{this.props.segment.element[i]}</span>
+                } else {
+                    select = <span>[empty]</span>
+                }
+                
+            }
             tabs.push(<Tab key={this.props.segment.name + (i + 1)}>{this.props.segment.name + (i + 1)}</Tab>);
             tabPanels.push(
-            <TabPanel key={this.props.segment.name + (i + 1)}>
+            <TabPanel key={this.props.segment.name + (i + 1)} className="tab-panel">
                  <div>Description: {details.description} ({v.requirementType })</div>
-                 <div>Data: {this.props.segment.element[i]}</div>
+                 <div>Data:{select}</div>
                  <div>Standard: {details.name} {details.dataType} {details.minLength}/{details.maxLength}</div>
             </TabPanel>);
         })
