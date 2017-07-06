bd22c9c6fa0dcf181df7d46fbfd0fac645d16b38
Nuwan Chandrasoma <nuwan.chandrasoma@gtnexus.com>
2017-07-06 15:49:06 +0530

    updated

--- src/components/templates/Group.js
+++ src/components/templates/Group.js
@@ -7,7 +7,23 @@ class Group extends Component {
         let elements = segment.element.map((v,i)=>{
             if (segment.schema && segment.schema.element) {
                 let details = EdiHelper.getSchemaDetails(segment.schema.element[i].name);
-                return <div key={i}><span>{details.description}</span><span>:</span><span>{v}</span></div>
+                if (details.name.startsWith("code")) {
+                    if (details.value) {
+                        let code;
+                        for (let i=0,length = details.value.length;i < length; i++) {
+                            let el = details.value[i];
+                            if (el.value === v) {
+                                code = el;
+                                break;
+                            }
+                        }
+                        return <div key={i}><span>{details.description}</span><span>:</span><span title={code.value}>{code.description}</span></div>
+                    } else {
+                        return <div key={i}><span>{details.description}</span><span>:</span><span>{v}</span></div>
+                    }
+                } else {
+                    return <div key={i}><span>{details.description}</span><span>:</span><span>{v}</span></div>
+                }
             } else {
                 return <div></div>
             }
@@ -39,10 +55,10 @@ class Group extends Component {
         return (
             <div style={{marginLeft:'30px'}}>
             <section className="segment segment-marker">
-                <header>{segment.schema.description}</header>
+                <header className="title" style={{width : '90%'}}>{segment.schema.description}</header>
                 {elements}
             </section>
-            <table className="table table-bordered" style={{width:'70%'}}>
+            <table className="table table-bordered" style={{width:'90%'}}>
                 <thead>
                     <tr>{headers}</tr>
                 </thead>
